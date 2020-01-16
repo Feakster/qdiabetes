face5068280adc6e693f2859ae25af126ea6d780
Feakster <greatwhite@posteo.eu>
2020-01-16 16:08:55 +0000

    Started vectorising the functions
    - Finished writing the unit tests for the QDRC function.
    - Wrote a vectorised version of the QDRA function. This runs and gives the correct number of values in the output. However, it doesn't give the correct values at present.

---
+++ R/vQDRA.R
@@ -0,0 +1,215 @@
+#=============================================#
+#                                             #
+#### QDIABETES-2018 (MODEL A) [VECTORISED] ####
+#                                             #
+#=============================================#
+
+### Notes ###
+# - age >= 25 & age < 85
+# - height >= 1.40 & height <= 2.10
+# - weight >= 40 & weight <= 180
+# - bmi == 20 if bmi < 20
+# - bmi == 40 if bmi > 40
+# - townsend >= -7.028634577 & townsend <= 13.3114711
+
+vQDRA <- function(gender = NULL, age = NULL, bmi = NULL, height = NULL, weight = NULL, ethnicity = "WhiteNA", smoking = "Non", townsend = 0, antipsy = FALSE, steroids = FALSE, cvd = FALSE, gestdiab = FALSE, learndiff = FALSE, schizobipo = FALSE, pcos = FALSE, statins = FALSE, hypertension = FALSE, fh_diab = FALSE){
+  
+  ## Stop Conditions ##
+  if(any(c(is.null(gender), is.null(age)))) stop("gender & age must be specified")
+  if(is.null(bmi) & any(c(is.null(height), is.null(weight)))) stop("either bmi or height & weight must be specified")
+  stopifnot(all(gender %in% c("Female", "Male")))
+  stopifnot(all(ethnicity %in% c("WhiteNA", "Indian", "Pakistani", "Bangladeshi", "OtherAsian", "BlackCaribbean", "BlackAfrican", "Chinese", "Other")))
+  stopifnot(all(smoking %in% c("Non", "Ex", "Light", "Moderate", "Heavy")))
+  stopifnot(all(age >= 25 & age < 85))
+  stopifnot(all(height >= 1.4 & height <= 2.1))
+  stopifnot(all(weight >= 40 & weight <= 180))
+  stopifnot(all(townsend >= -7.028634577 & townsend <= 13.3114711))
+  stopifnot(all(antipsy %in% c(FALSE, TRUE)))
+  stopifnot(all(steroids %in% c(FALSE, TRUE)))
+  stopifnot(all(cvd %in% c(FALSE, TRUE)))
+  stopifnot(all(gestdiab %in% c(FALSE, TRUE)))
+  stopifnot(all(learndiff %in% c(FALSE, TRUE)))
+  stopifnot(all(schizobipo %in% c(FALSE, TRUE)))
+  stopifnot(all(pcos %in% c(FALSE, TRUE)))
+  stopifnot(all(statins %in% c(FALSE, TRUE)))
+  stopifnot(all(hypertension %in% c(FALSE, TRUE)))
+  stopifnot(all(fh_diab %in% c(FALSE, TRUE)))
+  
+  ## Input Lengths ##
+  inputs <- list(gender, age, bmi, height, weight, ethnicity, smoking, townsend, antipsy, steroids, cvd, gestdiab, learndiff, schizobipo, pcos, statins, hypertension, fh_diab)
+  inputs_length <- sapply(inputs, length)
+  n <- max(inputs_length)
+  stopifnot(all(inputs_length %in% c(0:1, n)))
+  
+  ## BMI Pre-Procession ##
+  if(any(!is.na(bmi) & !is.na(height) & !is.na(weight))) warning("bmi, height & weight all specified, height & weight ignored", call. = FALSE)
+  if (is.null(bmi)) bmi <- weight/height^2
+  bmi[is.na(bmi)] <- weight/height^2
+  rm(height, weight)
+  stopifnot(all(bmi >= 40/2.1^2 & bmi <= 180/1.4^2))
+  if(any(bmi < 20)){
+    warning("bmi < 20. Setting bmi == 20", call. = FALSE)
+    bmi[bmi < 20] <- 20
+  }
+  if(any(bmi > 40)){
+    warning("bmi > 40. Setting bmi == 40", call. = FALSE)
+    bmi[bmi > 40] <- 40
+  }
+  
+  ## Harmonize Input Lengths ##
+  vec_extend <- function(x, n){
+    if(!is.null(x) & length(x) == 1) x <- rep(x, n) else x
+    return(x)
+  }
+  gender <- vec_extend(gender, n)
+  age <- vec_extend(age, n)
+  bmi <- vec_extend(bmi, n)
+  ethnicity <- vec_extend(ethnicity, n)
+  smoking <- vec_extend(smoking, n)
+  townsend <- vec_extend(townsend, n)
+  antipsy <- vec_extend(antipsy, n)
+  steroids <- vec_extend(steroids, n)
+  cvd <- vec_extend(cvd, n)
+  gestdiab <- vec_extend(gestdiab, n)
+  learndiff <- vec_extend(learndiff, n)
+  schizobipo <- vec_extend(schizobipo, n)
+  pcos <- vec_extend(pcos, n)
+  statins <- vec_extend(statins, n)
+  hypertension <- vec_extend(hypertension, n)
+  fh_diab <- vec_extend(fh_diab, n)
+  
+  ## Intermediate Vectors ##
+  vec_eth <- rep(NA_real_, n)
+  vec_eth[ethnicity == "WhiteNA"] <- 0
+  vec_smok <- rep(NA_real_, n)
+  vec_smok[smoking == "Non"] <- 0
+  dage <- age/10
+  age_1 <- rep(NA_real_, n)
+  age_2 <- rep(NA_real_, n)
+  dbmi <- bmi/10
+  bmi_1 <- rep(NA_real_, n)
+  bmi_2 <- rep(NA_real_, n)
+  risk <- rep(NA_real_, n)
+  
+  ## Gender Indices ##
+  ind_f <- which(gender == "Female")
+  ind_m <- which(gender == "Male")
+  
+  ## Female ##
+  # Ethnicity #
+  vec_eth[gender == "Female" & ethnicity == "Indian"] <- 1.0695857881565456
+  vec_eth[gender == "Female" & ethnicity == "Pakistani"] <- 1.3430172097414006
+  vec_eth[gender == "Female" & ethnicity == "Bangladeshi"] <- 1.8029022579794518
+  vec_eth[gender == "Female" & ethnicity == "OtherAsian"] <- 1.127465451770802
+  vec_eth[gender == "Female" & ethnicity == "BlackCaribbean"] <- 0.42146314902399101
+  vec_eth[gender == "Female" & ethnicity == "BlackAfrican"] <- 0.2850919645908353
+  vec_eth[gender == "Female" & ethnicity == "Chinese"] <- 0.88151087975891995
+  vec_eth[gender == "Female" & ethnicity == "Other"] <- 0.36605733431684873
+  
+  # Smoking #
+  vec_smok[gender == "Female" & smoking == "Ex"] <- 0.065601690175059055
+  vec_smok[gender == "Female" & smoking == "Light"] <- 0.28450988673698374
+  vec_smok[gender == "Female" & smoking == "Moderate"] <- 0.3567664381700702
+  vec_smok[gender == "Female" & smoking == "Heavy"] <- 0.53595171106787753
+  
+  # Age #
+  age_1[ind_f] <- dage[ind_f]^0.5
+  age_2[ind_f] <- dage[ind_f]^3
+  age_1[ind_f] <- age_1[ind_f] - 2.123332023620606
+  age_2[ind_f] <- age_2[ind_f] - 91.644744873046875
+  age[ind_f] <- 4.3400852699139278*age_1[ind_f] - 0.0048771702696158879*age_2[ind_f]
+  
+  # BMI #
+  bmi_1[ind_f] <- dbmi[ind_f]
+  bmi_2[ind_f] <- dbmi[ind_f]^3
+  bmi_1[ind_f] <- bmi_1[ind_f] - 2.571253299713135
+  bmi_2[ind_f] <- bmi_2[ind_f] - 16.999439239501953
+  bmi[ind_f] <- 2.9320361259524925*bmi_1[ind_f] - 0.04740020587484349*bmi_2[ind_f]
+  
+  # Townsend #
+  townsend[ind_f] <- townsend[ind_f] - 0.391116052865982
+  townsend[ind_f] <- 0.037340569618049151*townsend[ind_f]
+  
+  # Binary Variables #
+  antipsy[ind_f] <- 0.55267646110984381*antipsy[ind_f]
+  steroids[ind_f] <- 0.26792233680674599*steroids[ind_f]
+  cvd[ind_f] <- 0.17797229054586691*cvd[ind_f]
+  gestdiab[ind_f] <- 1.5248871531467574*gestdiab[ind_f]
+  learndiff[ind_f] <- 0.27835143587172717*learndiff[ind_f]
+  schizobipo[ind_f] <- 0.26180852109179059*schizobipo[ind_f]
+  pcos[ind_f] <- 0.34061739882066661*pcos[ind_f]
+  statins[ind_f] <- 0.65907287732808217*statins[ind_f]
+  hypertension[ind_f] <- 0.43947582858137119*hypertension[ind_f]
+  fh_diab[ind_f] <- 0.53133594565587339*fh_diab[ind_f]
+  
+  # Interaction Terms #
+  antipsy[ind_f] <- -0.80315183983163951*age_1[ind_f]*antipsy[ind_f] + 0.00046840411810210498*age_2[ind_f]*antipsy[ind_f]
+  learndiff[ind_f] <- -0.86415960028820571*age_1[ind_f]*learndiff[ind_f] + 0.00067249688089533602*age_2[ind_f]*learndiff[ind_f]
+  statins[ind_f] <- -1.9757776696583935*age_1[ind_f]*statins[ind_f] + 0.0023750534194347966*age_2[ind_f]*statins[ind_f]
+  bmi_1[ind_f] <- 0.65531387575629452*age_1[ind_f]*bmi_1[ind_f] - 0.0044719662445263054*age_2[ind_f]*bmi_1[ind_f]
+  bmi_2[ind_f] <- -0.036209657201630177*age_1[ind_f]*bmi_2[ind_f] + 0.0001185479967753342*age_2[ind_f]*bmi_2[ind_f]
+  fh_diab[ind_f] <- -0.26411714505588962*age_1[ind_f]*fh_diab[ind_f] + 0.00041610258289047683*age_2[ind_f]*fh_diab[ind_f]
+  
+  ## Male ##
+  # Ethnicity #
+  vec_eth[gender == "Male" & ethnicity == "Indian"] <- 1.1000230829124793
+  vec_eth[gender == "Male" & ethnicity == "Pakistani"] <- 1.290384012614721
+  vec_eth[gender == "Male" & ethnicity == "Bangladeshi"] <- 1.6740908848727458
+  vec_eth[gender == "Male" & ethnicity == "OtherAsian"] <- 1.1400446789147816
+  vec_eth[gender == "Male" & ethnicity == "BlackCaribbean"] <- 0.46824681690655806
+  vec_eth[gender == "Male" & ethnicity == "BlackAfrican"] <- 0.69905649963015448
+  vec_eth[gender == "Male" & ethnicity == "Chinese"] <- 0.68943657127111568
+  vec_eth[gender == "Male" & ethnicity == "Other"] <- 0.41722228467738209
+  
+  # Smoking #
+  vec_smok[gender == "Male" & smoking == "Ex"] <- 0.16387409105485573
+  vec_smok[gender == "Male" & smoking == "Light"] <- 0.31851449113958979
+  vec_smok[gender == "Male" & smoking == "Moderate"] <- 0.32207266567783432
+  vec_smok[gender == "Male" & smoking == "Heavy"] <- 0.45052437163409531
+  
+  # Age #
+  age_1[ind_m] <- log(dage[ind_m])
+  age_2[ind_m] <- dage[ind_m]^3
+  age_1[ind_m] <- age_1[ind_m] - 1.496392488479614
+  age_2[ind_m] <- age_2[ind_m] - 89.048171997070313
+  age[ind_m] <- 4.4642324388691348*age_1[ind_m] - 0.0040750108019255568*age_2[ind_m]
+  
+  # BMI #
+  bmi_1[ind_m] <- dbmi[ind_m]^2
+  bmi_2[ind_m] <- dbmi[ind_m]^3
+  bmi_1[ind_m] <- bmi_1[ind_m] - 6.817805767059326
+  bmi_2[ind_m] <- bmi_2[ind_m] - 17.801923751831055
+  bmi[ind_m] <- 0.95129027867120675*bmi_1[ind_m] - 0.14352488277885475*bmi_2[ind_m]
+  
+  # Townsend #
+  townsend[ind_m] <- townsend[ind_m] - 0.515986680984497
+  townsend[ind_m] <- 0.025918182067678725*townsend[ind_m]
+  
+  # Binary Variables #
+  antipsy[ind_m] <- 0.42101092346005436*antipsy[ind_m]
+  steroids[ind_m] <- 0.22183580932925384*steroids[ind_m]
+  cvd[ind_m] <- 0.20269605756290021*cvd[ind_m]
+  learndiff[ind_m] <- 0.23315321407986961*learndiff[ind_m]
+  schizobipo[ind_m] <- 0.22770449520517727*schizobipo[ind_m]
+  statins[ind_m] <- 0.58490075431141342*statins[ind_m]
+  hypertension[ind_m] <- 0.33379392183501078*hypertension[ind_m]
+  fh_diab[ind_m] <- 0.64799284899369536*fh_diab[ind_m]
+  
+  # Interaction Terms #
+  antipsy[ind_m] <- -0.94637722268534152*age_1[ind_m]*antipsy[ind_m] - 0.0000519927442172335*age_2[ind_m]*antipsy[ind_m]
+  learndiff[ind_m] <- -0.93842375526499833*age_1[ind_m]*learndiff[ind_m] + 0.00071026438559688141*age_2[ind_m]*learndiff[ind_m]
+  statins[ind_m] <- -1.7479070653003299*age_1[ind_m]*statins[ind_m] + 0.0013508364599531669*age_2[ind_m]*statins[ind_m]
+  bmi_1[ind_m] <- 0.45147599241879766*age_1[ind_m]*bmi_1[ind_m] - 0.0011797722394560309*age_2[ind_m]*bmi_1[ind_m]
+  bmi_2[ind_m] <- -0.10795481262776381*age_1[ind_m]*bmi_2[ind_m] + 0.00021471509139319291*age_2[ind_m]*bmi_2[ind_m]
+  fh_diab[ind_m] <- -0.60118530429301198*age_1[ind_m]*fh_diab[ind_m] + 0.00049141855940878034*age_2[ind_m]*fh_diab[ind_m]
+  
+  ## Risk Score ##
+  ethnicity <- vec_eth; rm(vec_eth)
+  smoking <- vec_smok; rm(vec_smok)
+  score <- ethnicity + smoking + bmi + age + townsend + antipsy + steroids + cvd + gestdiab + learndiff + schizobipo + pcos + statins + hypertension + fh_diab
+  risk[ind_f] <- 100*(1 - 0.986227273941040^exp(score[ind_f]))
+  risk[ind_m] <- 100*(1 - 0.978732228279114^exp(score[ind_m]))
+  
+  ## Output ##
+  return(risk)
+}
--- tests/testthat/test_QDRC.R
+++ tests/testthat/test_QDRC.R
@@ -161,106 +161,106 @@ expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(ag
 rm(list = ls(pattern = "^(risk|vec)_"))
 rm(tQDRC)
 
-# ## BMI ##
-# tQDRC <- function(x){gQDRC(bmi = x, age = 60, hba1c = 31.5)}
-# vec_bmi <- 90/seq(1.4, 2.1, 0.1)^2
-# risk_web <- c()
-# names(risk_web) <- round(vec_bmi, 1)
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_bmi, tQDRC)
-# })
-# names(risk_fun) <- round(vec_bmi, 1)
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(bmi)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## BMI ##
+tQDRC <- function(x){gQDRC(bmi = x, age = 60, hba1c = 31.5)}
+vec_bmi <- 90/seq(1.4, 2.1, 0.1)^2
+risk_web <- c(2.3, 2.3, 1.8, 1.3, 0.9, 0.6, 0.4, 0.3)
+names(risk_web) <- round(vec_bmi, 1)
 
-# ## Height ##
-# tQDRC <- function(x){gQDRC(height = x, age = 60, weight = 90, hba1c = 31.5)}
-# vec_ht <- seq(1.4, 2.1, 0.1)
-# risk_web <- c()
-# names(risk_web) <- vec_ht
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_ht, tQDRC)
-# })
-# names(risk_fun) <- vec_ht
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(height)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+suppressWarnings({
+  risk_fun <- sapply(vec_bmi, tQDRC)
+})
+names(risk_fun) <- round(vec_bmi, 1)
 
-# ## Weight ##
-# tQDRC <- function(x){gQDRC(weight = x, age = 60, height = 1.83, hba1c = 31.5)}
-# vec_wt <- seq(40, 180, 20)
-# risk_web <- c()
-# names(risk_web) <- vec_wt
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_wt, tQDRC)
-# })
-# names(risk_fun) <- vec_wt
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(weight)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(bmi)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
 
-# ## HbA1c ##
-# tQDRC <- function(x){gQDRC(hba1c = x, age = 60, height = 1.83, weight = 90)}
-# vec_hba1c <- seq(15, 45, 5)
-# risk_web <- c()
-# names(risk_web) <- vec_hba1c
-# 
-# risk_fun <- sapply(vec_hba1c, tQDRC)
-# names(risk_fun) <- vec_hba1c
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(hba1c)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## Height ##
+tQDRC <- function(x){gQDRC(height = x, age = 60, weight = 90, hba1c = 31.5)}
+vec_ht <- seq(1.4, 2.1, 0.1)
+risk_web <- c(2.3, 2.3, 1.8, 1.3, 0.9, 0.6, 0.4, 0.3)
+names(risk_web) <- vec_ht
+
+suppressWarnings({
+  risk_fun <- sapply(vec_ht, tQDRC)
+})
+names(risk_fun) <- vec_ht
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(height)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## Weight ##
+tQDRC <- function(x){gQDRC(weight = x, age = 60, height = 1.83, hba1c = 31.5)}
+vec_wt <- seq(40, 180, 20)
+risk_web <- c(0.3, 0.3, 0.5, 1.1, 1.9, 2.3, 2.3, 2.3)
+names(risk_web) <- vec_wt
+
+suppressWarnings({
+  risk_fun <- sapply(vec_wt, tQDRC)
+})
+names(risk_fun) <- vec_wt
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(weight)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## HbA1c ##
+tQDRC <- function(x){gQDRC(hba1c = x, age = 60, height = 1.83, weight = 90)}
+vec_hba1c <- seq(15, 45, 5)
+risk_web <- c(0, 0, 0.1, 0.5, 1.8, 5.2, 13.6)
+names(risk_web) <- vec_hba1c
+
+risk_fun <- sapply(vec_hba1c, tQDRC)
+names(risk_fun) <- vec_hba1c
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(hba1c)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
 
 ### Categorical Variables ###
-# ## Ethnicity ##
-# tQDRC <- function(x){gQDRC(ethnicity = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
-# expect_error(tQDRC(x = "Blue"), label = "QDRC-Female [ethnicity = 'Blue']")
-# vec_eth <- c("WhiteNA", "Indian", "Pakistani", "Bangladeshi", "OtherAsian", "BlackCaribbean", "BlackAfrican", "Chinese", "Other")
-# risk_web <- c()
-# names(risk_web) <- vec_eth
-# 
-# risk_fun <- sapply(vec_eth, tQDRC)
-# names(risk_fun) <- vec_eth
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(ethnicity)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## Ethnicity ##
+tQDRC <- function(x){gQDRC(ethnicity = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
+expect_error(tQDRC(x = "Blue"), label = "QDRC-Female [ethnicity = 'Blue']")
+vec_eth <- c("WhiteNA", "Indian", "Pakistani", "Bangladeshi", "OtherAsian", "BlackCaribbean", "BlackAfrican", "Chinese", "Other")
+risk_web <- c(0.8, 1.4, 1.7, 2.6, 1.6, 0.9, 0.8, 1.4, 0.9)
+names(risk_web) <- vec_eth
 
-# ## Smoking ##
-# tQDRC <- function(x){gQDRC(smoking = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
-# expect_error(tQDRC(x = "Maybe"), label = "QDRC-Female [smoking = 'Maybe']")
-# vec_smo <- c("Non", "Ex", "Light", "Moderate", "Heavy")
-# risk_web <- c()
-# names(risk_web) <- vec_smo
-# 
-# risk_fun <- sapply(vec_smo, tQDRC)
-# names(risk_fun) <- vec_smo
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(smoking)]", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+risk_fun <- sapply(vec_eth, tQDRC)
+names(risk_fun) <- vec_eth
 
-# ### Binary Variables ###
-# tQDRC <- function(...){gQDRC(age = 60, height = 1.83, weight = 90, hba1c = 31.5, ...)}
-# expect_equal(tQDRC(antipsy = T), , tolerance = tol, label = "QDRC-Female [antipsy = T]")
-# expect_equal(tQDRC(steroids = T), , tolerance = tol, label = "QDRC-Female [steroids = T]")
-# expect_equal(tQDRC(cvd = T), , tolerance = tol, label = "QDRC-Female [cvd = T]")
-# expect_equal(tQDRC(gestdiab = T), , tolerance = tol, label = "QDRC-Female [gestdiab = T]")
-# expect_equal(tQDRC(learndiff = T), , tolerance = tol, label = "QDRC-Female [learndiff = T]")
-# expect_equal(tQDRC(schizobipo = T), , tolerance = tol, label = "QDRC-Female [schizobipo = T]")
-# expect_equal(tQDRC(pcos = T), , tolerance = tol, label = "QDRC-Female [pcos = T]")
-# expect_equal(tQDRC(statins = T), , tolerance = tol, label = "QDRC-Female [statins = T]")
-# expect_equal(tQDRC(hypertension = T), , tolerance = tol, label = "QDRC-Female [hypertension = T]")
-# expect_equal(tQDRC(fh_diab = T), , tolerance = tol, label = "QDRC-Female [fh_diab = T]")
-# rm(tQDRC)
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(ethnicity)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## Smoking ##
+tQDRC <- function(x){gQDRC(smoking = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
+expect_error(tQDRC(x = "Maybe"), label = "QDRC-Female [smoking = 'Maybe']")
+vec_smo <- c("Non", "Ex", "Light", "Moderate", "Heavy")
+risk_web <- c(0.8, 0.8, 0.9, 0.9, 1.1)
+names(risk_web) <- vec_smo
+
+risk_fun <- sapply(vec_smo, tQDRC)
+names(risk_fun) <- vec_smo
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Female [range(smoking)]", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+### Binary Variables ###
+tQDRC <- function(...){gQDRC(age = 60, height = 1.83, weight = 90, hba1c = 31.5, ...)}
+expect_equal(tQDRC(antipsy = T), 1.1, tolerance = tol, label = "QDRC-Female [antipsy = T]")
+expect_equal(tQDRC(steroids = T), 0.9, tolerance = tol, label = "QDRC-Female [steroids = T]")
+expect_equal(tQDRC(cvd = T), 0.9, tolerance = tol, label = "QDRC-Female [cvd = T]")
+expect_equal(tQDRC(gestdiab = T), 2.4, tolerance = tol, label = "QDRC-Female [gestdiab = T]")
+expect_equal(tQDRC(learndiff = T), 0.9, tolerance = tol, label = "QDRC-Female [learndiff = T]")
+expect_equal(tQDRC(schizobipo = T), 1.1, tolerance = tol, label = "QDRC-Female [schizobipo = T]")
+expect_equal(tQDRC(pcos = T), 1.1, tolerance = tol, label = "QDRC-Female [pcos = T]")
+expect_equal(tQDRC(statins = T), 0.9, tolerance = tol, label = "QDRC-Female [statins = T]")
+expect_equal(tQDRC(hypertension = T), 1.2, tolerance = tol, label = "QDRC-Female [hypertension = T]")
+expect_equal(tQDRC(fh_diab = T), 1.2, tolerance = tol, label = "QDRC-Female [fh_diab = T]")
+rm(tQDRC)
 
 ### Tidy Up ###
 rm(gQDRC)
@@ -380,118 +380,118 @@ expect_error(tQDRC(hypertension = -1), label = "QDRC-Male [!{hypertension %in% c
 expect_error(tQDRC(fh_diab = -1), label = "QDRC-Male [!{fh_diab %in% c(0, 1, F, T)}]")
 rm(tQDRC)
 
-### Numerical Values ###
-# ## Age ##
-# tQDRC <- function(x){gQDRC(age = x, height = 1.83, weight = 90, hba1c = 31.5)}
-# vec_age <- seq(25, 80, 5)
-# risk_web <- c()
-# names(risk_web) <- vec_age
-# 
-# risk_fun <- sapply(vec_age, tQDRC)
-# names(risk_fun) <- vec_age
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (age)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## Numerical Values ###
+## Age ##
+tQDRC <- function(x){gQDRC(age = x, height = 1.83, weight = 90, hba1c = 31.5)}
+vec_age <- seq(25, 80, 5)
+risk_web <- c(0.1, 0.2, 0.4, 0.6, 0.8, 1.1, 1.3, 1.5, 1.5, 1.5, 1.3, 1.1)
+names(risk_web) <- vec_age
 
-# ## BMI ##
-# tQDRC <- function(x){gQDRC(bmi = x, age = 60, hba1c = 31.5)}
-# vec_bmi <- 90/seq(1.4, 2.1, 0.1)^2
-# risk_web <- c()
-# names(risk_web) <- round(vec_bmi, 1)
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_bmi, tQDRC)
-# })
-# names(risk_fun) <- round(vec_bmi, 1)
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (bmi)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+risk_fun <- sapply(vec_age, tQDRC)
+names(risk_fun) <- vec_age
 
-# ## Height ##
-# tQDRC <- function(x){gQDRC(height = x, age = 60, weight = 90, hba1c = 31.5)}
-# vec_ht <- seq(1.4, 2.1, 0.1)
-# risk_web <- c()
-# names(risk_web) <- vec_ht
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_ht, tQDRC)
-# })
-# names(risk_fun) <- vec_ht
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (height)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (age)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
 
-# ## Weight ##
-# tQDRC <- function(x){gQDRC(weight = x, age = 60, height = 1.83, hba1c = 31.5)}
-# vec_wt <- seq(40, 180, 20)
-# risk_web <- c()
-# names(risk_web) <- vec_wt
-# 
-# suppressWarnings({
-#   risk_fun <- sapply(vec_wt, tQDRC)
-# })
-# names(risk_fun) <- vec_wt
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (weight)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## BMI ##
+tQDRC <- function(x){gQDRC(bmi = x, age = 60, hba1c = 31.5)}
+vec_bmi <- 90/seq(1.4, 2.1, 0.1)^2
+risk_web <- c(5, 5, 3.9, 2.6, 1.7, 1.1, 0.7, 0.5)
+names(risk_web) <- round(vec_bmi, 1)
 
-# ## HbA1c ##
-# tQDRC <- function(x){gQDRC(hba1c = x, age = 60, height = 1.83, weight = 90)}
-# vec_hba1c <- 2:6
-# risk_web <- c()
-# names(risk_web) <- vec_hba1c
-# 
-# risk_fun <- sapply(vec_hba1c, tQDRC)
-# names(risk_fun) <- vec_hba1c
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (hba1c)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+suppressWarnings({
+  risk_fun <- sapply(vec_bmi, tQDRC)
+})
+names(risk_fun) <- round(vec_bmi, 1)
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (bmi)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## Height ##
+tQDRC <- function(x){gQDRC(height = x, age = 60, weight = 90, hba1c = 31.5)}
+vec_ht <- seq(1.4, 2.1, 0.1)
+risk_web <- c(5, 5, 3.9, 2.6, 1.7, 1.1, 0.7, 0.5)
+names(risk_web) <- vec_ht
+
+suppressWarnings({
+  risk_fun <- sapply(vec_ht, tQDRC)
+})
+names(risk_fun) <- vec_ht
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (height)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## Weight ##
+tQDRC <- function(x){gQDRC(weight = x, age = 60, height = 1.83, hba1c = 31.5)}
+vec_wt <- seq(40, 180, 20)
+risk_web <- c(0.5, 0.5, 0.9, 2.2, 4.1, 5, 5, 5)
+names(risk_web) <- vec_wt
+
+suppressWarnings({
+  risk_fun <- sapply(vec_wt, tQDRC)
+})
+names(risk_fun) <- vec_wt
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (weight)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## HbA1c ##
+tQDRC <- function(x){gQDRC(hba1c = x, age = 60, height = 1.83, weight = 90)}
+vec_hba1c <- seq(15, 45, 5)
+risk_web <- c(0, 0.1, 0.3, 1.1, 3, 7.5, 17)
+names(risk_web) <- vec_hba1c
+
+risk_fun <- sapply(vec_hba1c, tQDRC)
+names(risk_fun) <- vec_hba1c
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (hba1c)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
 
 ### Categorical Variables ###
-# ## Ethnicity ##
-# tQDRC <- function(x){gQDRC(ethnicity = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
-# expect_error(tQDRC(x = "Blue"), label = "QDRC-Male [ethnicity = 'Blue']")
-# vec_eth <- c("WhiteNA", "Indian", "Pakistani", "Bangladeshi", "OtherAsian", "BlackCaribbean", "BlackAfrican", "Chinese", "Other")
-# risk_web <- c()
-# names(risk_web) <- vec_eth
-# 
-# risk_fun <- sapply(vec_eth, tQDRC)
-# names(risk_fun) <- vec_eth
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (ethnicity)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+## Ethnicity ##
+tQDRC <- function(x){gQDRC(ethnicity = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
+expect_error(tQDRC(x = "Blue"), label = "QDRC-Male [ethnicity = 'Blue']")
+vec_eth <- c("WhiteNA", "Indian", "Pakistani", "Bangladeshi", "OtherAsian", "BlackCaribbean", "BlackAfrican", "Chinese", "Other")
+risk_web <- c(1.5, 2.9, 3.3, 4.3, 3.1, 1.8, 2.1, 2.1, 1.8)
+names(risk_web) <- vec_eth
 
-# ## Smoking ##
-# tQDRC <- function(x){gQDRC(smoking = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
-# expect_error(tQDRC(x = "Maybe"), label = "QDRC-Male [smoking = 'Maybe']")
-# vec_smo <- c("Non", "Ex", "Light", "Moderate", "Heavy")
-# risk_web <- c()
-# names(risk_web) <- vec_smo
-# 
-# risk_fun <- sapply(vec_smo, tQDRC)
-# names(risk_fun) <- vec_smo
-# 
-# expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (smoke)", expected.label = "ClinRisk")
-# rm(list = ls(pattern = "^(risk|vec)_"))
-# rm(tQDRC)
+risk_fun <- sapply(vec_eth, tQDRC)
+names(risk_fun) <- vec_eth
 
-# ### Binary Variables ###
-# tQDRC <- function(...){gQDRC(age = 60, height = 1.83, weight = 90, hba1c = 31.5, ...)}
-# expect_equal(tQDRC(antipsy = T), , tolerance = tol, label = "QDRC-Male [antipsy = T]")
-# expect_equal(tQDRC(steroids = T), , tolerance = tol, label = "QDRC-Male [steroids = T]")
-# expect_equal(tQDRC(cvd = T), , tolerance = tol, label = "QDRC-Male [cvd = T]")
-# expect_equal(tQDRC(learndiff = T), , tolerance = tol, label = "QDRC-Male [learndiff = T]")
-# expect_equal(tQDRC(schizobipo = T), , tolerance = tol, label = "QDRC-Male [schizobipo = T]")
-# expect_equal(tQDRC(statins = T), , tolerance = tol, label = "QDRC-Male [statins = T]")
-# expect_equal(tQDRC(hypertension = T), , tolerance = tol, label = "QDRC-Male [hypertension = T]")
-# expect_equal(tQDRC(fh_diab = T), , tolerance = tol, label = "QDRC-Male [fh_diab = T]")
-# rm(tQDRC)
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (ethnicity)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+## Smoking ##
+tQDRC <- function(x){gQDRC(smoking = x, age = 60, height = 1.83, weight = 90, hba1c = 31.5)}
+expect_error(tQDRC(x = "Maybe"), label = "QDRC-Male [smoking = 'Maybe']")
+vec_smo <- c("Non", "Ex", "Light", "Moderate", "Heavy")
+risk_web <- c(1.5, 1.6, 1.7, 1.6, 1.8)
+names(risk_web) <- vec_smo
+
+risk_fun <- sapply(vec_smo, tQDRC)
+names(risk_fun) <- vec_smo
+
+expect_equal(risk_fun, risk_web, tolerance = tol, label = "QDRC-Male Risk (smoke)", expected.label = "ClinRisk")
+rm(list = ls(pattern = "^(risk|vec)_"))
+rm(tQDRC)
+
+### Binary Variables ###
+tQDRC <- function(...){gQDRC(age = 60, height = 1.83, weight = 90, hba1c = 31.5, ...)}
+expect_equal(tQDRC(antipsy = T), 1.8, tolerance = tol, label = "QDRC-Male [antipsy = T]")
+expect_equal(tQDRC(steroids = T), 1.7, tolerance = tol, label = "QDRC-Male [steroids = T]")
+expect_equal(tQDRC(cvd = T), 1.7, tolerance = tol, label = "QDRC-Male [cvd = T]")
+expect_equal(tQDRC(learndiff = T), 1.6, tolerance = tol, label = "QDRC-Male [learndiff = T]")
+expect_equal(tQDRC(schizobipo = T), 1.9, tolerance = tol, label = "QDRC-Male [schizobipo = T]")
+expect_equal(tQDRC(statins = T), 1.6, tolerance = tol, label = "QDRC-Male [statins = T]")
+expect_equal(tQDRC(hypertension = T), 2, tolerance = tol, label = "QDRC-Male [hypertension = T]")
+expect_equal(tQDRC(fh_diab = T), 2.3, tolerance = tol, label = "QDRC-Male [fh_diab = T]")
+rm(tQDRC)
 
 ### Tidy Up ###
 rm(gQDRC)
