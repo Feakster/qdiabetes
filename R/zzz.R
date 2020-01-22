11b64e9d7499708e89b6542a7c9aacc41913cd83
Feakster <greatwhite@posteo.eu>
2020-01-22 14:50:23 +0000

    Final commit before the big switchover to vectorisation
    - Last commit before I erase all the old serialised versions of the algorithm.
    - Added tests to check that the vectorised verstion of the QDRC() function work as expected.
    - Reformatted the package load message.

--- R/zzz.R
+++ R/zzz.R
@@ -7,9 +7,12 @@
 .onAttach <- function(libname, pkgname){
   setHook(packageEvent("QDiabetes", "attach"), function(...){
     packageStartupMessage(
-      "Contains OS data © Crown copyright and database right 2020",
-      "Contains Royal Mail data © Royal Mail copyright and database right 2020",
-      "Source: Office for National Statistics licensed under the Open Government Licence v.3.0"
+      paste(
+        "Contains OS data © Crown copyright and database right 2019",
+        "Contains Royal Mail data © Royal Mail copyright and database right 2019",
+        "Source: Office for National Statistics licensed under the Open Government Licence v.3.0",
+        sep = "\n"
+      )
     )
   })
 }
