662ce79974b4aa39c7ffbf59cbd4aa6ae63e8bd9
Benjamin Feakins <greatwhite@posteo.eu>
2020-02-02 20:27:55 +0000

    Minor tweaks and ToDos
    - Fixed a bug whereby the package  startup messages would duplicate each time the package was detached, then re-attached via library().
    - Added a feature idea.

--- R/zzz.R
+++ R/zzz.R
@@ -9,14 +9,12 @@
 }
 
 .onAttach <- function(libname, pkgname){
-  setHook(packageEvent("QDiabetes", "attach"), function(...){
-    packageStartupMessage(
-      paste(
-        "Contains OS data Crown copyright and database right 2019",
-        "Contains Royal Mail data Royal Mail copyright and database right 2019",
-        "Source: Office for National Statistics licensed under the Open Government Licence v.3.0",
-        sep = "\n"
-      )
+  packageStartupMessage(
+    paste(
+      "Contains OS data Crown copyright and database right 2019",
+      "Contains Royal Mail data Royal Mail copyright and database right 2019",
+      "Source: Office for National Statistics licensed under the Open Government Licence v.3.0",
+      sep = "\n"
     )
-  })
+  )
 }
